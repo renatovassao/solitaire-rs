d70f77655483ad51ca9a1342905e569f6066aff8
Renato Matsuda Vassão Dias <renato.dias@cubos.io>
2020-03-14 21:39:43 -0300

    test new methods

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,15 +1,19 @@
 use game::{Game, DealSize};
+// use deck::{Suit};
 
 fn main() {
-    let mut game = Game::new(DealSize::Three);
-    game.deal();
-    game.deal();
-    game.deal();
-    game.deal();
-    game.deal();
-    game.deal();
-    game.deal();
-    game.deal();
-    game.deal();
+    let mut game = Game::new(DealSize::One);
+    let mut oks: Vec<bool> = Vec::new();
+
+    for _ in 0..2 {
+        game.deal();
+    }
+    // for _ in 0..10 {
+    //     oks.push(game.waste_to_foundation(Suit::Diamonds));
+    // }
+
+    oks.push(game.waste_to_tableau(2));
+
     println!("{:#?}", game);
+    println!("oks = {:#?}", oks);
 }
