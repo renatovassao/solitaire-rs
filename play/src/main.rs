e8b79005a36744c2b19177956e74a217299174f0
Renato Matsuda Vassão Dias <renato.dias@cubos.io>
2020-03-18 00:25:03 -0300

    test: create game loop and test game display

--- play/src/main.rs
+++ play/src/main.rs
@@ -1,19 +1,34 @@
 use game::{Game, DealSize};
-// use deck::{Suit};
+use deck::{Suit};
+use std::io;
 
 fn main() {
     let mut game = Game::new(DealSize::One);
     let mut oks: Vec<bool> = Vec::new();
 
-    for _ in 0..2 {
+    for _ in 0..11 {
         game.deal();
     }
-    // for _ in 0..10 {
-    //     oks.push(game.waste_to_foundation(Suit::Diamonds));
-    // }
+    for _ in 0..1 {
+        oks.push(game.waste_to_foundation(Suit::Diamonds));
+    }
+    for _ in 0..133 {
+        game.deal();
+    }
+    for _ in 0..13 {
+        oks.push(game.waste_to_foundation(Suit::Clubs));
+    }
 
-    oks.push(game.waste_to_tableau(2));
+    loop {
+        print!("\x1B[2J");
+        println!("SOLITAIRE\n\n\n\n\n");
+        println!("{}", game);
+        println!("Type a command: ");
 
-    println!("{:#?}", game);
-    println!("oks = {:#?}", oks);
+        let mut command = String::new();
+
+        io::stdin().read_line(&mut command)
+            .expect("Failed to read line");
+    }
+    // println!("oks = {:#?}", oks);
 }
